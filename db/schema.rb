195329454c6353d1efa94c50afce7c08ac08250a
Justin Taj <justintaj@yahoo.com>
2018-11-15 14:04:07 -0800

    Added color and price to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_001323) do
+ActiveRecord::Schema.define(version: 2018_11_15_212411) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_11_09_001323) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
+    t.string "price"
   end
 
 end
