8cad0851ad9fb777b9ccae1c9af76433f7e83d21
Justin Taj <justintaj@yahoo.com>
2018-11-14 21:55:10 -0800

    implementing products lesson 4.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # this is how the active link reflects which page youre on. Directions available in message transcript with Bill on CareerFoundry
+  def current_class?(test_path)
+    return 'active' if request.path == test_path
+    ''
+  end
 end
