6c89b288d51f5ef2c22f0a877b3474e0154e33cf
Justin Taj <justintaj@yahoo.com>
2018-11-27 17:09:45 -0800

    added carousel, product partial and adjusted carousel indicators to reflect the quantity of products featured. I also was able to make the inidicators work according to the product number by using #{index} but i still cant get the prev and next indicators to work

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,8 +1,10 @@
 class SimplePagesController < ApplicationController
   def index
   end
-  
+
   def landing_page
     @featured_product = Product.first
+    @products = Product.limit(5)
+
   end
 end
