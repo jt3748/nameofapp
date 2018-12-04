782922b7300b19e03073fe08f446f02920221e13
Justin Taj <justintaj@yahoo.com>
2018-12-04 15:28:33 -0800

    5.1 adjusted tasks to work so that heroku can be pushed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to "/simple_pages/contact", notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
       if @product.update(product_params)
         # format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.html { redirect_back @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
