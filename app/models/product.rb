c2e1c091c5ffd1a8eb59e510926844af16d61ed0
Justin Taj <justintaj@yahoo.com>
2018-12-16 16:33:31 -0800

    4.2 corrections(make LIKE to iLIKE in production)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,10 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
+    if Rails.env.development?
     Product.where("name LIKE ?", "%#{search_term}%")
-  end
+  elseif Rails.env.production?
+    Product.where("name iLIKE ?", "%#{search_term}%")
+    end
 
 end
