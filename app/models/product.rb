d3a12bfa8a108be883e836479569d65df2f0d7ce
Justin Taj <justintaj@yahoo.com>
2018-12-16 16:55:22 -0800

    fixed syntax in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,8 @@ class Product < ApplicationRecord
   def self.search(search_term)
     if Rails.env.development?
     Product.where("name LIKE ?", "%#{search_term}%")
-  elseif Rails.env.production?
+  elsif Rails.env.production?
     Product.where("name ilike ?", "%#{search_term}%")
     end
-
+  end
 end
