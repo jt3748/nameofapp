41e1aa8fcc8157ad89e1812931320aae4b3dbd43
Justin Taj <justintaj@yahoo.com>
2019-01-03 14:34:22 -0800

    5.8 added raty but its not working, updated products.show, added partials for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
