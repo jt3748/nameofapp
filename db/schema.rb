5112b7ff8cffec7bed82d49a77877a1db55fe623
Justin Taj <justintaj@yahoo.com>
2019-01-08 13:58:11 -0800

    added comments migration via master branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_234316) do
+ActiveRecord::Schema.define(version: 2019_01_08_212414) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
