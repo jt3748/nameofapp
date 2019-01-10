36afcb27251c6caa581d354984ceb3e4de26f579
Justin Taj <justintaj@yahoo.com>
2019-01-10 10:17:12 -0800

    5.3 trial and error with SendGrid. Its not sending the messages.changed email

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -14,10 +14,10 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     UserMailer.contact_form(@email, @name, @message).deliver_now
-    # ActionMailer::Base.mail(from: @email,
-    #     to: 'your-email@example.com',
-    #     subject: "A new contact form message from #{@name}",
-    #     body: @message).deliver_now
+    ActionMailer::Base.mail(from: @email,
+        to: 'your-email@example.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
     #ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
   end
 end
