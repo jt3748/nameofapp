8b733ecce11163c496efc3e6c85a901906acb627
Justin Taj <justintaj@yahoo.com>
2019-01-10 09:49:17 -0800

    removed code from line 21 of simplepagescontroller.rb to test if thats the cause of the emails not being sent to the correct address.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -17,7 +17,7 @@ class SimplePagesController < ApplicationController
     # ActionMailer::Base.mail(from: @email,
     #     to: 'your-email@example.com',
     #     subject: "A new contact form message from #{@name}",
-    #     body: @message).deliver_now 
-    ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
+    #     body: @message).deliver_now
+    #ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
   end
 end
