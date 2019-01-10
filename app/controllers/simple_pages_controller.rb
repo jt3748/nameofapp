e20001e1676aef54779e8a474e8b7764d337ce43
Justin Taj <justintaj@yahoo.com>
2019-01-10 10:25:03 -0800

    5.3 mod. changed to address

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -15,7 +15,7 @@ class SimplePagesController < ApplicationController
     @message = params[:message]
     UserMailer.contact_form(@email, @name, @message).deliver_now
     ActionMailer::Base.mail(from: @email,
-        to: 'your-email@example.com',
+        to: 'justin.taj@gmail.com',
         subject: "A new contact form message from #{@name}",
         body: @message).deliver_now
     #ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
