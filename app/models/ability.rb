d29afef2c0dfccd05c2f42c39be636e1d9966a38
Justin Taj <justintaj@yahoo.com>
2019-01-14 21:12:14 -0800

    changed ability.rb user restrictions. changed user_controller_spec to make tests pass appropriately. Changed application_controller so that the rescue_from method redirects a user to the login page instead of home when there is a Cancan error such as when a user tried to edit another user.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
-    can :read, :all # permissions for every user, even if not logged in
-    if user.present?  # additional permissions for logged in users (they can manage their comments)
-      can :manage, Comment, user_id: user.id
+    can :read, Order, user_id: user.id # permissions for every user, even if not logged in
       if user.admin?  # additional permissions for administrators
         can :manage, :all
       end
     end
-  end
+    
 end
