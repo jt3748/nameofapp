fbe71cb8b00c7fba3e1c635315906e6ecaef5bd9
Justin Taj <justintaj@yahoo.com>
2019-01-15 16:22:46 -0800

    6.5 corrections: made product.price*100, fixed url so thumbnail shows on checkout, moved hidden tag to proper position

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,13 +1,13 @@
 class PaymentsController < ApplicationController
 
   def create
-    @product = params[:product_id]
+    @product = Product.find(params[:product_id])
     @user = current_user
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        amount: number_to_currency(product.price), # amount in cents, again
+        amount: product.price*100 # amount in cents, again
         currency: "usd",
         source: token,
         description: params[:stripeEmail],
