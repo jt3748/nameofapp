ec616d8828fdbf4cdf85c4238a4e0f05fbc3c74b
Justin Taj <justintaj@yahoo.com>
2019-01-16 12:26:35 -0800

    6.5 make price to integer, inputted STRIPE values on heroku

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,7 +7,7 @@ class PaymentsController < ApplicationController
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        amount: product.price*100, # amount in cents, again
+        amount: (@product.price*100).to_i, # amount in cents, again
         currency: "usd",
         source: token,
         description: params[:stripeEmail],
@@ -26,6 +26,6 @@ class PaymentsController < ApplicationController
       err = body[:error]
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}. Please try again."
     end
-    redirect_to product_path
+    redirect_to products_path
   end
 end
