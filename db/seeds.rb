10e47f5b46a6bf91e4a3b3eae0228c8ebf0d35de
Justin Taj <justintaj@yahoo.com>
2019-02-02 16:14:49 -0800

    changed products to cacti

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 Order.delete_all
 Comment.delete_all
 Product.delete_all
-Product.create(price: 12.99, name: "Wooden Ornament", description: "3x3inches", image_url: "art/ornament.jpg")
-Product.create(price: 85.99, name: "Stone Angel", description: "1x3feet", image_url: "art/babyangel.jpg")
-Product.create(price: 45.99, name: "Ceramic Angel", description: "4x6inches", image_url: "art/angel_ceramic.jpg")
-Product.create(price: 12.99, name: "Metal Figurines", description: "1.5x2.75inches", image_url: "art/monkey_figurines.jpg")
-Product.create(price: 65.99, name: "Acrylic Canvas", description: "3x3feet", image_url: "art/ocean_canvas.jpg")
-Product.create(price: 25.99, name: "Acrylic Canvas", description: "3x1.5feet", image_url: "art/cubism.jpg")
-Product.create(price: 15.99, name: "3D Printed Skull", description: "0.5x0.5feet", image_url: "10.jpg")
-Product.create(price: 25.99, name: "Charcoal Portrait", description: "1x0.5feet", image_url: "art/charcoal-portrait.png")
+Product.create(price: 15.99, name: "Item 1", description: "Insert product description here", image_url: "plant12.jpeg")
+Product.create(price: 15.99, name: "Item 5", description: "Insert product description here", image_url: "plant6.png")
+Product.create(price: 15.99, name: "Item 6", description: "Insert product description here", image_url: "plant7.png")
+Product.create(price: 15.99, name: "Item 7", description: "Insert product description here", image_url: "plant8.png")
+Product.create(price: 15.99, name: "Item 9", description: "Insert product description here", image_url: "plant10.png")
+Product.create(price: 15.99, name: "Item 10", description: "Insert product description here", image_url: "plant11.png")
+Product.create(price: 15.99, name: "Item 11", description: "Insert product description here", image_url: "plant2.png")
+Product.create(price: 15.99, name: "Item 12", description: "Insert product description here", image_url: "plant3.png")
