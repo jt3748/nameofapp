6749610598446589de0de541cca7aa10e23d92e3
Justin Taj <justintaj@yahoo.com>
2019-02-05 18:14:08 -0800

    redesigned landing page. shrunk carousel images to fit.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,13 +1,13 @@
 class SimplePagesController < ApplicationController
   def index
-    
+
     @products = Product.limit(5)
     @featured_product = Product.first
   end
 
   def landing_page
 
-    @products = Product.limit(2)
+    @products = Product.limit(5)
     @featured_product = Product.first
   end
 
