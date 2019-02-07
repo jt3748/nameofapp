21e261567a059e19149f2090772cdadd18fdea68
Justin Taj <justintaj@yahoo.com>
2019-02-06 21:28:32 -0800

    removed column and table dropping migrations for heroku because they were never ran so therefore cant be dropped

--- db/migrate/20190207041654_drop_order_items.rb
+++
@@ -1,5 +0,0 @@
-class DropOrderItems < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :order_items
-  end
-end
--- db/migrate/20190207041728_drop_order_statuses.rb
+++
@@ -1,5 +0,0 @@
-class DropOrderStatuses < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :order_statuses
-  end
-end
